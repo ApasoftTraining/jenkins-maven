e1e69e4b609b7385183815413bc619d31fcf6a05
Apasoft Training <54261225+ApasoftTraining@users.noreply.github.com>
2024-05-05 16:25:36 +0200

    Update Sum.java

--- src/main/java/com/apasoft/Sum.java
+++ src/main/java/com/apasoft/Sum.java
@@ -1,23 +1,26 @@
 public class Sum {
     public static void main(String[] args) {
         try {
-            if (args.length == 0) {
-                num1=1;
-				num2=1;
+            int num1;
+            int num2;
+            if (args.length < 2) {
+                 num1=1;
+                num2=1;
             }
-			else
-			{
+        else
+        {
             // Convert parameters from String to integers
-            int num1 = Integer.parseInt(args[0]);
-            int num2 = Integer.parseInt(args[1]);
-			}
+             num1 = Integer.parseInt(args[0]);
+             num2 = Integer.parseInt(args[1]);
+        }
             // Sum the numbers
             int result = num1 + num2;
-
             // Print result
-            System.out.println("The sum of" + num1 + " and " + num2 + " is " + resul);
+            System.out.println("The sum of" + num1 + " and " + num2 + " is " + result);
         } catch (NumberFormatException e) {
             System.out.println("Error: The parameteres must be integers.");
         }
     }
 }
+
+
