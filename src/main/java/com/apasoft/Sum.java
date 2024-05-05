a9ba347cc5d4f88bb71e901944ac7b609a03d8fe
Apasoft Training <54261225+ApasoftTraining@users.noreply.github.com>
2024-05-05 16:10:58 +0200

    Update Sum.java

--- src/main/java/com/apasoft/Sum.java
+++ src/main/java/com/apasoft/Sum.java
@@ -1,15 +1,16 @@
 public class Sum {
     public static void main(String[] args) {
         try {
-            if (args.length < 2) {
-                System.out.println("I need two numbers to sum.");
-                return;
+            if (args.length == 0) {
+                num1=1;
+				num2=1;
             }
-
+			else
+			{
             // Convert parameters from String to integers
             int num1 = Integer.parseInt(args[0]);
             int num2 = Integer.parseInt(args[1]);
-
+			}
             // Sum the numbers
             int result = num1 + num2;
 
